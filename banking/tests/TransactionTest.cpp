875de2df800d0c5bc3d0c0491da8ef5e1ef05bad
neddsr <nneddsr@gmail.com>
2025-05-19 18:08:55 +0300

    Update TransactionTest.cpp

--- banking/tests/TransactionTest.cpp
+++ banking/tests/TransactionTest.cpp
@@ -1,132 +1,123 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include "Transaction.h"
 #include "Account.h"
+#include "Transaction.h"
 
+using ::testing::AtLeast;
 using ::testing::Return;
+using ::testing::Ref;
 using ::testing::_;
-using ::testing::Exactly;
+using ::testing::HasSubstr;
+using ::testing::NiceMock;
 
 class MockAccount : public Account {
- public:
-  MOCK_METHOD(int, id, (), (const, override));
-  MOCK_METHOD(void, Lock, (), (override));
-  MOCK_METHOD(void, Unlock, (), (override));
-  MOCK_METHOD(int, GetBalance, (), (const, override));
-  MOCK_METHOD(void, ChangeBalance, (int), (override));
+public:
+    MockAccount(int id, int balance) : Account(id, balance) {}
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int diff), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
 };
 
-class TransactionTest : public ::testing::Test {
- protected:
-  Transaction transaction_;
-  MockAccount from_;
-  MockAccount to_;
+class MockTransaction : public Transaction {
+public:
+    MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
 };
 
-TEST_F(TransactionTest, ThrowsIfSameAccount) {
-  EXPECT_CALL(from_, id()).WillRepeatedly(Return(1));
-  EXPECT_CALL(to_, id()).WillRepeatedly(Return(1));
-
-  EXPECT_THROW(transaction_.Make(from_, to_, 100), std::logic_error);
-}
-
-TEST_F(TransactionTest, ThrowsIfNegativeSum) {
-  EXPECT_CALL(from_, id()).WillRepeatedly(Return(1));
-  EXPECT_CALL(to_, id()).WillRepeatedly(Return(2));
-
-  EXPECT_THROW(transaction_.Make(from_, to_, -10), std::invalid_argument);
-}
-
-TEST_F(TransactionTest, ThrowsIfSumTooSmall) {
-  EXPECT_CALL(from_, id()).WillRepeatedly(Return(1));
-  EXPECT_CALL(to_, id()).WillRepeatedly(Return(2));
-
-  EXPECT_THROW(transaction_.Make(from_, to_, 50), std::logic_error);
-}
-
-TEST_F(TransactionTest, ReturnsFalseIfFeeTooHigh) {
-  EXPECT_CALL(from_, id()).WillRepeatedly(Return(1));
-  EXPECT_CALL(to_, id()).WillRepeatedly(Return(2));
-
-  EXPECT_FALSE(transaction_.Make(from_, to_, 1));
-}
-
-TEST_F(TransactionTest, ReturnsFalseIfInsufficientFunds) {
-  EXPECT_CALL(from_, id()).WillRepeatedly(Return(1));
-  EXPECT_CALL(to_, id()).WillRepeatedly(Return(2));
-
-  EXPECT_CALL(from_, Lock()).Times(1);
-  EXPECT_CALL(to_, Lock()).Times(1);
-  EXPECT_CALL(from_, Unlock()).Times(1);
-  EXPECT_CALL(to_, Unlock()).Times(1);
-
-  EXPECT_CALL(from_, GetBalance()).WillOnce(Return(100)); 
-  EXPECT_CALL(to_, ChangeBalance(_)).Times(0);
-  EXPECT_CALL(from_, ChangeBalance(_)).Times(0);
-
-  bool result = transaction_.Make(from_, to_, 100);
-  EXPECT_FALSE(result);
-}
-
-TEST_F(TransactionTest, SuccessfulTransaction) {
-  EXPECT_CALL(from_, id()).WillRepeatedly(Return(1));
-  EXPECT_CALL(to_, id()).WillRepeatedly(Return(2));
-
-  EXPECT_CALL(from_, Lock()).Times(1);
-  EXPECT_CALL(to_, Lock()).Times(1);
-  EXPECT_CALL(from_, Unlock()).Times(1);
-  EXPECT_CALL(to_, Unlock()).Times(1);
-
-  EXPECT_CALL(from_, GetBalance()).WillOnce(Return(200));
-
-
-  EXPECT_CALL(to_, ChangeBalance(100)).Times(1);
-
-
-  EXPECT_CALL(from_, ChangeBalance(-101)).Times(1);
-
-  bool result = transaction_.Make(from_, to_, 100);
-  EXPECT_TRUE(result);
-}
-
-TEST_F(TransactionTest, DebitFailsAfterCreditRollback) {
-  EXPECT_CALL(from_, id()).WillRepeatedly(Return(1));
-  EXPECT_CALL(to_, id()).WillRepeatedly(Return(2));
-
-  EXPECT_CALL(from_, Lock()).Times(1);
-  EXPECT_CALL(to_, Lock()).Times(1);
-  EXPECT_CALL(from_, Unlock()).Times(1);
-  EXPECT_CALL(to_, Unlock()).Times(1);
-
-  EXPECT_CALL(from_, GetBalance()).WillOnce(Return(150));  
-
-  EXPECT_CALL(to_, ChangeBalance(100)).Times(1);
-
+TEST(TransactionTests, SuccessfulTransaction) {
+    const int initial1 = 1000;
+    const int initial2 = 2000;
+    const int sum = 500;
+    
+    NiceMock<MockAccount> acc1(1, initial1);
+    NiceMock<MockAccount> acc2(2, initial2);
+    MockTransaction transaction;
+    
+  
+    ON_CALL(acc1, GetBalance()).WillByDefault(Return(initial1));
+    ON_CALL(acc2, GetBalance()).WillByDefault(Return(initial2));
+    
  
-  EXPECT_CALL(from_, GetBalance()).WillOnce(Return(50)); 
-  EXPECT_CALL(from_, ChangeBalance(_)).Times(0);
-  EXPECT_CALL(to_, ChangeBalance(-100)).Times(1); 
+    EXPECT_CALL(acc1, Lock()).Times(1);
+    EXPECT_CALL(acc2, Lock()).Times(1);
+    
+ 
+    EXPECT_CALL(acc2, ChangeBalance(sum)).Times(1);
+    
+ 
+    EXPECT_CALL(acc1, ChangeBalance(-(sum + transaction.fee()))).Times(1);
+    
 
-  bool result = transaction_.Make(from_, to_, 100);
-  EXPECT_FALSE(result);
+    EXPECT_CALL(acc1, Unlock()).Times(1);
+    EXPECT_CALL(acc2, Unlock()).Times(1);
+    
+
+    EXPECT_CALL(transaction, SaveToDataBase(Ref(acc1), Ref(acc2), sum)).Times(1);
+
+    EXPECT_TRUE(transaction.Make(acc1, acc2, sum));
 }
 
+TEST(TransactionTests, FailedTransactionDueToInsufficientFunds) {
+    const int initial1 = 100;
+    const int initial2 = 2000;
+    const int sum = 500;
+    
+    NiceMock<MockAccount> acc1(1, initial1);
+    NiceMock<MockAccount> acc2(2, initial2);
+    MockTransaction transaction;
+    
+ 
+    ON_CALL(acc1, GetBalance()).WillByDefault(Return(initial1));
+    ON_CALL(acc2, GetBalance()).WillByDefault(Return(initial2));
+    
 
-TEST_F(TransactionTest, SaveToDatabaseIsCalledOnSuccess) {
-  EXPECT_CALL(from_, id()).WillRepeatedly(Return(1));
-  EXPECT_CALL(to_, id()).WillRepeatedly(Return(2));
+    EXPECT_CALL(acc1, Lock()).Times(1);
+    EXPECT_CALL(acc2, Lock()).Times(1);
+    
 
-  EXPECT_CALL(from_, Lock()).Times(1);
-  EXPECT_CALL(to_, Lock()).Times(1);
-  EXPECT_CALL(from_, Unlock()).Times(1);
-  EXPECT_CALL(to_, Unlock()).Times(1);
+    EXPECT_CALL(acc2, ChangeBalance(sum)).Times(1);
+    
+  
+    EXPECT_CALL(acc2, ChangeBalance(-sum)).Times(1);
+    
+   
+    EXPECT_CALL(acc1, ChangeBalance(_)).Times(0);
+    
+  
+    EXPECT_CALL(acc1, Unlock()).Times(1);
+    EXPECT_CALL(acc2, Unlock()).Times(1);
+    
 
-  EXPECT_CALL(from_, GetBalance()).WillOnce(Return(200));
-  EXPECT_CALL(to_, ChangeBalance(100)).Times(1);
-  EXPECT_CALL(from_, ChangeBalance(-101)).Times(1);
+    EXPECT_CALL(transaction, SaveToDataBase(Ref(acc1), Ref(acc2), sum)).Times(1);
 
+    EXPECT_FALSE(transaction.Make(acc1, acc2, sum));
+}
 
-  bool result = transaction_.Make(from_, to_, 100);
-  EXPECT_TRUE(result);
+TEST(TransactionTests, DatabaseOutputFormat) {
+    const int initial1 = 1000;
+    const int initial2 = 2000;
+    const int sum = 500;
+
+    Transaction transaction;
+    
+  
+    NiceMock<MockAccount> acc1(1, initial1);
+    NiceMock<MockAccount> acc2(2, initial2);
+    
+ 
+    ON_CALL(acc1, GetBalance())
+        .WillByDefault(Return(initial1 - sum - transaction.fee()));
+    ON_CALL(acc2, GetBalance())
+        .WillByDefault(Return(initial2 + sum));
+    
+
+    testing::internal::CaptureStdout();
+    transaction.Make(acc1, acc2, sum);
+    std::string output = testing::internal::GetCapturedStdout();
+    
+
+    EXPECT_THAT(output, HasSubstr("1 send to 2 $500"));
+    EXPECT_THAT(output, HasSubstr("Balance 1 is 499"));
+    EXPECT_THAT(output, HasSubstr("Balance 2 is 2500"));
 }
