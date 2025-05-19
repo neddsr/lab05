e03143fd94cec3ea56ee5fad5ed91435839f3a89
neddsr <nneddsr@gmail.com>
2025-05-19 18:39:29 +0300

    Update TransactionTest.cpp

--- banking/tests/TransactionTest.cpp
+++ banking/tests/TransactionTest.cpp
@@ -9,6 +9,7 @@ using ::testing::Return;
 using ::testing::Ref;
 using ::testing::_;
 using ::testing::HasSubstr;
+using ::testing::NiceMock;
 
 class MockAccount : public Account {
 public:
@@ -29,26 +30,23 @@ TEST(TransactionTests, SuccessfulTransaction) {
     const int initial2 = 2000;
     const int sum = 500;
     
-    MockAccount acc1(1, initial1);
-    MockAccount acc2(2, initial2);
+    NiceMock<MockAccount> acc1(1, initial1);
+    NiceMock<MockAccount> acc2(2, initial2);
     MockTransaction transaction;
-
+    
+   
+    
+    testing::InSequence seq;
+    
     EXPECT_CALL(acc1, Lock()).Times(1);
     EXPECT_CALL(acc2, Lock()).Times(1);
     
-
     EXPECT_CALL(acc2, ChangeBalance(sum)).Times(1);
-    
-
     EXPECT_CALL(acc1, ChangeBalance(-(sum + transaction.fee()))).Times(1);
     
-
-    EXPECT_CALL(acc1, GetBalance()).WillOnce(Return(initial1));
-    
-
     EXPECT_CALL(acc1, Unlock()).Times(1);
     EXPECT_CALL(acc2, Unlock()).Times(1);
-   
+    
     EXPECT_CALL(transaction, SaveToDataBase(Ref(acc1), Ref(acc2), sum)).Times(1);
 
     EXPECT_TRUE(transaction.Make(acc1, acc2, sum));
@@ -59,27 +57,28 @@ TEST(TransactionTests, FailedTransactionDueToInsufficientFunds) {
     const int initial2 = 2000;
     const int sum = 500;
     
-    MockAccount acc1(1, initial1);
-    MockAccount acc2(2, initial2);
+    NiceMock<MockAccount> acc1(1, initial1);
+    NiceMock<MockAccount> acc2(2, initial2);
     MockTransaction transaction;
+    
+    testing::InSequence seq;
     
     EXPECT_CALL(acc1, Lock()).Times(1);
     EXPECT_CALL(acc2, Lock()).Times(1);
     
- 
+    
     EXPECT_CALL(acc2, ChangeBalance(sum)).Times(1);
     
-
-    EXPECT_CALL(acc1, GetBalance()).WillOnce(Return(initial1));
    
+    ON_CALL(acc1, GetBalance()).WillByDefault(Return(initial1));
+    
+  
     EXPECT_CALL(acc2, ChangeBalance(-sum)).Times(1);
-
-    EXPECT_CALL(acc1, ChangeBalance(_)).Times(0);
     
     EXPECT_CALL(acc1, Unlock()).Times(1);
     EXPECT_CALL(acc2, Unlock()).Times(1);
     
-   
+    
     EXPECT_CALL(transaction, SaveToDataBase(Ref(acc1), Ref(acc2), sum)).Times(1);
 
     EXPECT_FALSE(transaction.Make(acc1, acc2, sum));
@@ -91,25 +90,20 @@ TEST(TransactionTests, DatabaseOutputFormat) {
     const int sum = 500;
     
     Transaction transaction;
-    MockAccount acc1(1, initial1);
-    MockAccount acc2(2, initial2);
+    NiceMock<MockAccount> acc1(1, initial1);
+    NiceMock<MockAccount> acc2(2, initial2);
     
-
-    ON_CALL(acc1, GetBalance()).WillByDefault(Return(initial1 - sum - transaction.fee()));
-    ON_CALL(acc2, GetBalance()).WillByDefault(Return(initial2 + sum));
-    
- 
-    EXPECT_CALL(acc1, Lock()).Times(1);
-    EXPECT_CALL(acc2, Lock()).Times(1);
-    EXPECT_CALL(acc2, ChangeBalance(sum)).Times(1);
-    EXPECT_CALL(acc1, ChangeBalance(-(sum + transaction.fee()))).Times(1);
-    EXPECT_CALL(acc1, Unlock()).Times(1);
-    EXPECT_CALL(acc2, Unlock()).Times(1);
+   
+    ON_CALL(acc1, GetBalance())
+        .WillByDefault(Return(initial1 - sum - transaction.fee()));
+    ON_CALL(acc2, GetBalance())
+        .WillByDefault(Return(initial2 + sum));
     
     testing::internal::CaptureStdout();
     transaction.Make(acc1, acc2, sum);
     std::string output = testing::internal::GetCapturedStdout();
-
+    
+ 
     EXPECT_THAT(output, HasSubstr("1 send to 2 $500"));
     EXPECT_THAT(output, HasSubstr("Balance 1 is 499")); 
     EXPECT_THAT(output, HasSubstr("Balance 2 is 2500"));
